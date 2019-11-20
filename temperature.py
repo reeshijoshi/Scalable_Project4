70ba0bdbfb2030fcb649493f31ba7f0a53bbd939
reeshijoshi <reeshijoshi@gmail.com>
2019-11-20 14:01:33 +0000

    Modified file names to reflect who it belongs to

--- temperature.py
+++ temperature.py
@@ -4,12 +4,14 @@ import time
 import os
 from random import randint
 import botocore.config
+import BatteryHandling
+import sys
 
-tempFile = "D:/Study/Project4/tempFile.json"
-sensorID = "TemperatureSensor_1"
+tempFile = "D:/Documents/AWSIOT/tempFile.json"
+sensorID = "TemperatureSensor_2"
 sensorType = "Temperature"
-functionNameAggregator = 'arn:aws:lambda:eu-west-1:023947881979:function:sendReceiveDataHopefully'
-functionNamePedometer = 'arn:aws:lambda:eu-west-1:023947881979:function:pedometerSecondaryAggregator'
+functionNameAggregator = 'arn:aws:lambda:eu-west-1:094482812955:function:sensorToAggregator'
+functionNamePedometer = 'arn:aws:lambda:eu-west-1:094482812955:function:pedometerAggregator'
 invocationType = 'RequestResponse'
 sensor = {"SensorType" : sensorType}
 value = 0
@@ -17,6 +19,11 @@ message = ""
 alert = False
 data = {}
 cfg = botocore.config.Config(connect_timeout = 5, read_timeout = 5)
+energy = 0
+powerType = 1
+threshold1 = 20
+threshold2 = 10
+
 class SomethingWentWrong(Exception):
     pass
 def updatePayload(data):
@@ -32,16 +39,24 @@ def updatePayload(data):
     return data
 
 def tryConnection(data, functionName):
+    global energy
     client = boto3.client("lambda", config = cfg)
     try:
         data.update(sensor)
+        payload=json.dumps(data)
         print("Request Payload = " + str(data))
+        request_size = sys.getsizeof(payload)
+        startTime = int(round(time.time() * 1000))
         response = client.invoke(
         FunctionName=functionName,
         InvocationType=invocationType,
         LogType='Tail',
-        Payload=json.dumps(data),
+        Payload=payload,
         )
+        endTime = int(round(time.time() * 1000))
+        connectionDuration = endTime-startTime
+        energy = BatteryHandling.decrease_transfer_energy(connectionDuration, energy)
+        energy = BatteryHandling.increase_energy(energy, connectionDuration)
         responsePayload = json.loads(response['Payload'].read().decode("utf-8"))
         statusCode = int(responsePayload["statusCode"])
         if (statusCode != 200) :
@@ -54,13 +69,19 @@ def tryConnection(data, functionName):
         print(str(e))
         return False
 
+def writeToTempFile(data):
+    try:
+        with open(tempFile, "w+") as jsonFile:
+            json.dump(data, jsonFile)
+    except Exception as e:
+        print(str(e))
+
 def sendDataToAggregator(data):
     try:
         data = updatePayload(data)
         if not tryConnection(data, functionNameAggregator):
             if not tryConnection(data, functionNamePedometer):
-                with open(tempFile, "w+") as jsonFile:
-                    json.dump(data, jsonFile)
+                writeToTempFile(data)
         return True
     except Exception as e:
         print(str(e))
@@ -85,11 +106,33 @@ def checkLoadDataFromTempFile():
         with open(tempFile, "r") as jsonFile:
             data = json.load(jsonFile)
         os.remove(tempFile)
-    
 
+def sleepTimer(duration):
+    global energy
+    for i in range(duration):
+        time.sleep(1)
+        energy = BatteryHandling.decrease_idle_time_energy(energy)
+        print("\n*************************************Battery left (Idle time energy loss) - "+str(energy))
+        energy = BatteryHandling.increase_energy(energy)
+        print("\n*************************************Battery left (Recharging) - "+str(energy))
+
+energy = int(sys.argv[1])
 while True:
+    data = {}
     getCurrentReading()
     checkLoadDataFromTempFile()
-    sendDataToAggregator(data)
-    data = {}
-    time.sleep(10)
+    if energy <=threshold1:
+        print("\n*************************************Battery Left after txn - "+str(energy))
+        if energy <= threshold2:
+            writeToTempFile(data)
+            print("----------------------------Readings written to temp file")
+            sleepTimer(20)
+        else:
+            sendDataToAggregator(data)
+            sleepTimer(20)
+    else:
+        getCurrentReading()
+        checkLoadDataFromTempFile()
+        sendDataToAggregator(data)
+        print("\n*************************************Battery Left after txn - "+str(energy))
+        sleepTimer(10)
